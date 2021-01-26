69e32439f3df1a3dc373e8d284257fd6310dcbdc
gustavo <gustavodevaler@gmail.com>
2021-01-26 04:06:59 -0300

    fc

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark-blue">
+  <a class="navbar-brand" href="#">Tienda Tik</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        
+        <router-link to="/" class="nav-link">Home</router-link> 
+      </li>
+      <li class="nav-item active">
+        <router-link to="/about" class="nav-link">About</router-link>
+      </li>
+      <li class="nav-item active">
+       <router-link to="/about" class="nav-link">About</router-link>
+      </li>
+      <li class="nav-item active">
+       <router-link to="/about" class="nav-link">About</router-link>
+      </li>
+    </ul> 
+    <router-view/>
   </div>
-  <router-view/>
+</nav>
+ 
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.bg-dark-blue{
+  background-color: rgb(22, 3, 66);
 }
 </style>
